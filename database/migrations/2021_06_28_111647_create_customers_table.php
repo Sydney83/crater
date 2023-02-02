4efba1c43b562f382d1858b75ee96da2d97245ad
atish160384 <atish@beehyv.com>
2023-02-02 20:09:22 +0530

    BAH-2454 |  Refractored made_at column

--- database/migrations/2021_06_28_111647_create_customers_table.php
+++ database/migrations/2021_06_28_111647_create_customers_table.php
@@ -15,7 +15,7 @@ class CreateCustomersTable extends Migration
     {
         Schema::create('customers', function (Blueprint $table) {
             $table->id();
-            $table->string('made_at');
+            $table->string('made_at')->nullable();
             $table->string('name');
             $table->string('email')->unique()->nullable();
             $table->string('phone')->nullable();
