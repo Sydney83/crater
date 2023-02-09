2ca5308a6edcec6980733d0bee7c596909e20e61
Atish Sisodia <atish.sisodia.beehyv@alation.com>
2023-02-09 10:11:40 +0530

    BAH-2563 | Changed logout to button (#22)
    * BAH-2563 | Changed session driver to file in .env
    
    * BAH-2563 | Changed the session driver to database
    
    * BAH-2563 | Refractored session lifetime .
    
    * BAH-2563 | Refractored logout from link to button
    
    * BAH-2563 | update. workflow steps
    
    ---------
    
    Co-authored-by: atish160384 <atish@beehyv.com>
    Co-authored-by: Abinaya U <abinaya.u@thoughtworks.com>

--- resources/scripts/components/base/BaseDropdownItem.vue
+++ resources/scripts/components/base/BaseDropdownItem.vue
@@ -1,14 +1,11 @@
 <template>
   <MenuItem v-slot="{ active }" v-bind="$attrs">
-    <a
-      href="#"
-      :class="[
-        active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
-        'group flex items-center px-4 py-2 text-sm font-normal',
-      ]"
-    >
-      <slot :active="active" />
-    </a>
+    <button :class="[
+    active ? 'bg-gray-100 text-gray-900' : 'text-gray-700',
+    'group flex items-center px-4 py-2 text-sm font-normal',
+  ]" style="width: 100% !important">
+    <slot :active="active" />
+  </button>
   </MenuItem>
 </template>
 
