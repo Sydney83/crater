b1458c590e1432ccd4fd5e942f6c0d15e591b3d0
binduak <tbindu@thoughtworks.com>
2023-02-21 11:13:04 +0530

    Bindu | add logout method

--- app/Http/Controllers/V1/Admin/Auth/LoginController.php
+++ app/Http/Controllers/V1/Admin/Auth/LoginController.php
@@ -5,6 +5,7 @@ namespace Crater\Http\Controllers\V1\Admin\Auth;
 use Crater\Http\Controllers\Controller;
 use Crater\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -36,5 +37,16 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function logout(Request $request)
+    {
+        $this->guard()->logout();
+
+        $request->session()->flush();
+
+        $request->session()->regenerate();
+
+        return redirect('/login');
     }
 }
